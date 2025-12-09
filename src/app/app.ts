443d95ee4dc57e4b50b9dbff3dc88d40fc4631bb
maxmp-q <maxmpjp@gmail.com>
2025-12-09 19:00:08 +0100

    add login form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,20 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, computed, inject, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './components/header/header';
 import {AppState} from './store/state';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
   state = inject(AppState);
+
+  password = signal<string>('');
+  login = computed(()=> this.password() === "WM2025");
 
   ngOnInit() {
     this.state.loadUsers();
